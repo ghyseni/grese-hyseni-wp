4c7e530be3acf593401fd1ca1100e02af18ff97c
Grese Hyseni <ghyseni@hawk.iit.edu>
2018-03-21 23:00:15 +0100

    Kthimi i menyse kryesore 'nav' te paprogramuar

--- wp-content/themes/my-website/frontend/header.php
+++ wp-content/themes/my-website/frontend/header.php
@@ -2,26 +2,32 @@
 <html class="no-js" lang="en-US">
 
 <head>
-  <title><?php bloginfo('title'); ?></title>
-  <meta charset="<?php bloginfo('charset'); ?>" />
-  <meta name="viewport" content="width=device-width,initial-scale=1.0,shrink-to-fit=no" />
-  <link href="https://fonts.googleapis.com/css?family=Oswald" rel="stylesheet" />
-  <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet" />
-  <link rel="stylesheet" href="../css/screen.css" />
+    <title><?php bloginfo('title'); ?></title>
+    <meta charset="<?php bloginfo('charset'); ?>"/>
+    <meta name="viewport" content="width=device-width,initial-scale=1.0,shrink-to-fit=no"/>
+    <link href="https://fonts.googleapis.com/css?family=Oswald" rel="stylesheet"/>
+    <link href="https://fonts.googleapis.com/css?family=Lato" rel="stylesheet"/>
+    <link rel="stylesheet" href="../css/screen.css"/>
 </head>
 <body>
-  <header id="header">
+<header id="header">
     <h1><a href="../"><?php bloginfo('title'); ?></a></h1>
     <!-- .opened class will be added to #nav using a jquery funciton, to open the menu after clicking the menu button -->
     <nav id="nav">
-  <?php  wp_nav_menu( array( 'theme_location' => 'main-menu', 'class' => 'menu' ) ); ?>
-    <ul class="network">
-        <li><a class="facebook" href="#"></a></li>
-        <li><a class="linkedin" href="#"></a></li>
-    </ul>
+        <nav id="nav">
+            <ul class="menu">
+                <li><a href="">Home</a></li>
+                <li><a href="resume/">Resume</a></li>
+                <li><a href="portfolio/">Portfolio</a></li>
+            </ul>
+            <ul class="network">
+                <li><a class="facebook" href="#"></a></li>
+                <li><a class="linkedin" href="#"></a></li>
+            </ul>
+        </nav>
     </nav>
     <!-- .close class will be added using jquery function to change the shape of the menu icon to X after menu is opened -->
     <button id="show-nav">
-      <span class="button-line"></span>
+        <span class="button-line"></span>
     </button>
-  </header>
+</header>
--- wp-content/themes/my-website/frontend/portfolio.php
+++ wp-content/themes/my-website/frontend/portfolio.php
@@ -1,25 +1,4 @@
 <?php include 'header.php'; ?>
-
-<body>
-  <header id="header">
-    <h1><a href="../">Grese Hyseni</a></h1>
-    <!-- .opened class will be added to #nav using a jquery funciton, to open the menu after clicking the menu button -->
-    <nav id="nav">
-      <ul class="menu">
-        <li><a href="../">Home</a></li>
-        <li><a href="../resume/">Resume</a></li>
-        <li><a href="../portfolio/">Portfolio</a></li>
-      </ul>
-      <ul class="network">
-        <li><a class="facebook" href="#"></a></li>
-        <li><a class="linkedin" href="#"></a></li>
-      </ul>
-    </nav>
-    <!-- .close class will be added using jquery function to change the shape of the menu icon to X after menu is opened -->
-    <button id="show-nav">
-      <span class="button-line"></span>
-    </button>
-  </header>
   <main id="main" class="portfolio">
     <h2 class="page-title">Portfolio</h2>
     <ul class="portfolio-entries">
