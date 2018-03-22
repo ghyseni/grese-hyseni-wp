bf63f21382eb13e6cc39e939ac70a12ccbbfa66a
Grese Hyseni <ghyseni@hawk.iit.edu>
2018-03-22 22:39:51 +0100

    Foto ne home page eshte marre nga featured image e home page; Programimi i Skills section ne home page

--- wp-content/themes/my-website/functions.php
+++ wp-content/themes/my-website/functions.php
@@ -1,10 +1,17 @@
 <?php
-function register_my_menus() {
+/* Register Menus, without this your theme won't support Menu,
+and Appearance -> Menus won't be visible from the dashboard*/
+function register_my_menus()
+{
     register_nav_menus(
         array(
-            'main-menu' => __( 'Main Menu' ),
-            'social-media-menu' => __( 'Social Media Menu' )
+            'main-menu' => __('Main Menu'),
+            'social-media-menu' => __('Social Media Menu')
         )
     );
 }
-add_action( 'init', 'register_my_menus' );
+
+add_action('init', 'register_my_menus');
+
+/* Add theme support for post thumbnails */
+add_theme_support( 'post-thumbnails' );
--- wp-content/themes/my-website/index.php
+++ wp-content/themes/my-website/index.php
@@ -2,11 +2,11 @@
 
     <main id="main" class="about">
 
-        // The loop will show the content based on what you selected on Settings -> Reading: 'Your homepage displays'
-        // IF you selected 'static page' than this will show that page
-        // Whereas if you selected 'Your latest posts', it will go through your posts and display each
         <article class="content">
             <?php
+            // The loop will show the content based on what you selected on Settings -> Reading: 'Your homepage displays'
+            // IF you selected 'static page' than this will show that page
+            // Whereas if you selected 'Your latest posts', it will go through your posts and display each
             if (have_posts()) {
                 while (have_posts()) {
                     the_post();
@@ -16,44 +16,29 @@
             ?>
         </article>
 
-        // With WP_Query we will create a new query ( instead of the default - the one below while(havE_posts())
-        // We will specify the arguments based on which the query will bring the result
-
-        <?php
-        $args = array(
-            'post_type' => 'page',
-            
-        );
-        $query = new WP_Query( $args );
-
-        ?>
-
         <section class="skills content">
-            <h3>Skills</h3>
-            <ul>
-                <li>
-                    HTML5
-                </li>
-                <li>
-                    CSS3
-                </li>
-                <li>
-                    JS/Jquery
-                </li>
-                <li>
-                    PHP
-                </li>
-                <li>
-                    Wordpress
-                </li>
-                <li>
-                    NodeJS
-                </li>
-                <li>
-                    Java
-                </li>
-            </ul>
-            <span class="bg-image"></span>
+            <?php
+
+            // With WP_Query we will create a new query ( instead of the default - the one below while(havE_posts())
+            // We will specify the arguments based on which the query will bring the result
+            $args = array(
+                'post_type' => 'page',
+                'pagename' => 'skills'
+            );
+            $skills_query = new WP_Query($args);
+            if ($skills_query->have_posts()):
+                while ($skills_query->have_posts()):
+                    $skills_query->the_post();
+                    ?>
+                    <h3><?php the_title(); ?></h3>
+                    <?php
+                    the_content();
+                endwhile;
+            endif;
+            // This allows that every template tag (like the_post_thumbnail()) will refer to the main query not to the custom wuery ($skills_query)
+            wp_reset_postdata();
+            ?>
+            <span class="bg-image" style="background-image:url('<?php the_post_thumbnail_url('full'); ?>');"></span>
         </section>
 
     </main>
